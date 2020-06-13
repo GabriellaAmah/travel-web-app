bac765e46db1f272a47299ae4a9a4498dd5ec84a
gabbie amah <gabbieamah49@gmail.com>
2020-06-13 13:01:12 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+let cities = [
+    {url : '../images/image3.jpg', h1: 'explore the madives' },
+    {url : '../images/image4.jpg', h1: 'experience cair0' },
+    {url : '../images/image5.jpg', h1: 'beach bum in morroco' },
+    {url : '../images/image2.jpg', h1: 'resurrect your taste vuds' },
+    {url : '../images/image6.jpg', h1: 'explore the philipines' }
+]
+
+let next = document.querySelector('.next');
+let previous = document.querySelector('.previous');
+let image = document.querySelector('.image')
+
+let i = 0;
+
