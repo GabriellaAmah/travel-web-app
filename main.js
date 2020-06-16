b23249610c88d8b27d7a4581de3c4c7eee767fce
gabbie amah <gabbieamah49@gmail.com>
2020-06-16 09:10:07 +0100

    modified the buttons in js

--- main.js
+++ main.js
@@ -1,14 +1,37 @@
 let cities = [
-    {url : '../images/image3.jpg', h1: 'explore the madives' },
-    {url : '../images/image4.jpg', h1: 'experience cair0' },
-    {url : '../images/image5.jpg', h1: 'beach bum in morroco' },
-    {url : '../images/image2.jpg', h1: 'resurrect your taste vuds' },
-    {url : '../images/image6.jpg', h1: 'explore the philipines' }
+    {url : "url('../images/image3.jpg')", h1: 'explore the madives' },
+    {url : "url('../images/image4.jpg')", h1: 'experience cairo' },
+    {url : "url('../images/image5.jpg')", h1: 'beach bum in morroco' },
+    {url : "url('../images/image2.jpg')", h1: 'resurrect your taste buds' },
+    {url : "url('../images/image6.jpg')", h1: 'explore the philipines' }
 ]
 
 let next = document.querySelector('.next');
 let previous = document.querySelector('.previous');
 let image = document.querySelector('.image')
+let header = document.querySelector('.showCaseText')
 
-let i = 0;
+let index = 0;
+
+next.addEventListener('click', function(){
+    index++
+   if (index === cities.length){
+     index = 0
+   }
+
+     image.style.backgroundImage =`linear-gradient(rgba(0, 15, 200, 0.5), rgba(68, 74, 160, 0.5)), ${cities[index].url}`, 
+     header.textContent = cities[index].h1
+})
+
+previous.addEventListener('click', () => {
+    if(index === 0){
+        index = cities.length
+    }
+
+    index--;
+
+    image.style.backgroundImage =`linear-gradient(rgba(0, 15, 200, 0.5), rgba(68, 74, 160, 0.5)), ${cities[index].url}`, 
+     header.textContent = cities[index].h1
+
+})
 
